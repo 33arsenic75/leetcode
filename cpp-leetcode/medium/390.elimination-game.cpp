ed3ae517772b09be86bb3003e43410a90b782280
33Arsenic75 <122697564+33Arsenic75@users.noreply.github.com>
2024-04-08 20:03:56 +0530

    .

--- cpp-leetcode/medium/390.elimination-game.cpp
+++ cpp-leetcode/medium/390.elimination-game.cpp
@@ -8,7 +8,20 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-        
+        if(n==1)return 1;
+        int ord = 1;
+        int ans = 1;
+        int remain = n;
+        bool left = 0;
+        while(remain>1){
+            if(left==0 || remain%2 ==1 ){
+                    ans = ans + ord;
+            }
+            ord = ord*2;
+            remain= remain/2;
+            left = !left;
+        }
+        return ans;
     }
 };
 // @lc code=end
