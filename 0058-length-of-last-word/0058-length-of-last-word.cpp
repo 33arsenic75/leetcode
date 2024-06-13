7c18fd059be8c6732834aab312aa294f76945201
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-06-13 16:32:27 +0530

    Time: 0 ms (100%), Space: 7.6 MB (54.61%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        reverse(s.begin(),s.end());
+        int i = 0; int n = s.size();
+        while(i<n && s[i]==' ')i++;
+        int ans = 0;
+        while(i<n && s[i]!=' '){
+            i++;
+            ans++;
+        }
+        return ans;
+    }
+};
