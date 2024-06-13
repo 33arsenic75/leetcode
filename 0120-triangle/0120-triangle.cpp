3e0c79cde1a96f7a177d9edfec85a1e84b7160bb
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-06-13 21:41:12 +0530

    Time: 4 ms (73.17%), Space: 11.3 MB (57.13%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sz;
+    vector<vector<int>>trg;
+    void process(int idx){
+        if(idx<0)return;
+        for(int i = 0;i<idx+1;i++){
+            trg[idx][i] = trg[idx][i] + min(trg[idx+1][i],trg[idx+1][i+1]);
+        }
+        process(idx-1);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        sz = triangle.size();
+        trg = triangle;
+        process(sz-2);
+        return trg[0][0];
+    }
+};
