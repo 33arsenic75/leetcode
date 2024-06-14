f5122ba86438eb3533baaecc16d044edd77e56a3
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-06-14 16:02:37 +0530

    Time: 0 ms (100%), Space: 9.2 MB (97.12%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // int n;
+    // vector<vector<int>> dp;
+    // vector<int> a;
+    // void dpf(int i, int j) {
+    //     if(i>j)return;
+    //     if(dp[i][j]!=0)return;
+    //     dpf(i+2,j);
+    //     dpf(i,j-2);
+    //     dpf(i+1,j-1);
+    //     int temp = 0;
+    //     temp = max(temp , a[i] + dp[i+2][j]);
+    //     temp = max(temp , a[j] + dp[i][j-2]);
+    //     temp = max(temp , max(a[i],a[j]) + dp[i+1][j-1]);
+    //     dp[i][j] = temp;
+    // }
+    bool stoneGame(vector<int>& piles) {
+        // n = piles.size();
+        // a = piles;
+        // dp = vector<vector<int>>(n, vector<int>(n, 0));
+        // for(int i = 0 ; i < n - 1 ; i ++){
+        //     dp[i][i+1] = max(a[i],a[i+1]);
+        // }
+        // dpf(0,n-1);
+        // return 2*dp[0][n-1] > accumulate(a.begin(),a.end(),0);
+        return true;
+    }
+};
+/// a,b,R,c,d
+/// R c d
+// a b R 
+// 
