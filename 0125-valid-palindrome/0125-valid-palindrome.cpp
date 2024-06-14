2eaa84d7cf18594795d0ff0a32fe5c5e17cf2a79
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-06-14 10:09:25 +0530

    Time: 8 ms (34.54%), Space: 8.7 MB (61.17%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    char tolower(char c){
+        if('a'<=c && c<='z')return c;
+        else if('A'<=c && c<='Z')return c + 32;
+        else if('0'<=c && c<='9')return c;
+        else return ' ';
+    }
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int i = 0, j = n-1;
+        char c1, c2;
+        while(j>i){
+            c1 = tolower(s[i]);
+            c2 = tolower(s[j]);
+            if(c1==' ')i++;
+            else if(c2==' ')j--;
+            else if(c1==c2){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
