a49bcb10ae1d140769f55a99694d23ac4d5296e3
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-06-15 01:26:02 +0530

    Time: 3 ms (67.77%), Space: 7 MB (93.07%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        if(n==1){
+            return 1;
+        }
+        else{
+            return (findTheWinner(n-1,k)+k-1)%n+1;
+        }
+    }
+    int myjos(int n,int k){
+        return findTheWinner(n,k);
+    }
+};
