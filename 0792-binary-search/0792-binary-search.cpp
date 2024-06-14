790c5ba0a03ec4b138c82edcd6b58cd53da6d89e
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-06-14 16:00:50 +0530

    Time: 22 ms (86.7%), Space: 29.8 MB (66.33%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, h = nums.size()-1;
+        int m;
+        while(l<=h){
+            m = l + (h-l)/2;
+            cout<<l<<' '<<h<<' '<<m<<'\n';
+            if(nums[m]==target)return m;
+            else if(nums[m]>target)h=m-1;
+            else l=m+1;
+        }
+        return -1;
+    }
+};
