c155004b33395efeafd30a9679be45fb8414a7ba
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-06-14 10:14:08 +0530

    Time: 3 ms (90.38%), Space: 11.8 MB (81.5%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        vector<int>cnt(32,0);
+        for(int i:nums){
+            for(int j=0;j<32;j++){
+                if((i&(1<<j))!=0)cnt[j]++;
+            }
+        }
+        int ans = 0;
+        for(int i = 0;i<32;i++){
+            cout<<cnt[i];
+            ans+= (cnt[i]%3)*(1<<i);
+        }
+        return ans;
+    }
+};
