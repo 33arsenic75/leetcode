369bbf00e6e61cd085a853d25d586c38b5ce47a2
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-06-25 11:42:05 +0530

    Time: 412 ms (58.69%), Space: 11.5 MB (70.2%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    vector<long long>prm;
-    void help(long long n){
-        prm.push_back(2);
-        vector<long long>a(n,true);
-        for(long long i = 3 ;  i < n ; i+=2){
-            if(!a[i])continue;
-            prm.push_back(i);
-            for(long long j = i*i ; j < n ; j+=i){
-                a[j]=false;
+    int countPrimes(int n) {
+        vector<bool>prm(n,true);
+        long long cnt = 0;
+        for(long long i = 2; i < n ; i++){
+            if(prm[i]){
+                cnt++;
+                for(long long j = i*i ; j < n ; j+=i)prm[j]=false;
             }
         }
-    }
-    int countPrimes(int n) {
-        if(n<=2)return 0;
-        help(n);
-        return prm.size();
+        return cnt;
     }
 };
