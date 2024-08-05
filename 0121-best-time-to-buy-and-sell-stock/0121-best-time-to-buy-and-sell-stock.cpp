19429f13bcaf79f8d21c5500c15b97e692ec114c
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-08-05 16:50:31 +0530

    Update 0121-best-time-to-buy-and-sell-stock.cpp

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,3 +1,4 @@
+/// O(n) solution
 class Solution {
 public:
     vector<int>p;
@@ -23,3 +24,31 @@ public:
         return ans(0,p.size()-1);
     }
 };
+
+
+/// O(nlogn) solution
+// class Solution {
+// public:
+//     vector<int>p;
+//     int minv(int i,int j){
+//         if(j<i)return 1e9;
+//         return *min_element(p.begin()+i,p.begin()+j+1);
+//     }
+//     int maxv(int i,int j){
+//         if(j<i)return 0;
+//         return *max_element(p.begin()+i,p.begin()+j+1);
+//     }
+//     int ans(int i,int j){
+//         if(j<=i)return 0;
+//         int mid = i + (j-i)/2;
+//         int result = 0;
+//         result = max(result,ans(i,mid));
+//         result = max(result,ans(mid+1,j));
+//         result = max(result,maxv(mid+1,j)-minv(i,mid));
+//         return result;
+//     }
+//     int maxProfit(vector<int>& prices) {
+//         p = prices;
+//         return ans(0,p.size()-1);
+//     }
+// };
