10880f031bb373dfd1f9401776d97a2a74716e6b
Abhinav Rajesh Shripad <122697564+33Arsenic75@users.noreply.github.com>
2024-08-05 16:49:42 +0530

    Time: 112 ms (5.2%), Space: 100.1 MB (5.05%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
+    vector<int>p;
+    int minv(int i,int j){
+        if(j<i)return 1e9;
+        return *min_element(p.begin()+i,p.begin()+j+1);
+    }
+    int maxv(int i,int j){
+        if(j<i)return 0;
+        return *max_element(p.begin()+i,p.begin()+j+1);
+    }
+    int ans(int i,int j){
+        if(j<=i)return 0;
+        int mid = i + (j-i)/2;
+        int result = 0;
+        result = max(result,ans(i,mid));
+        result = max(result,ans(mid+1,j));
+        result = max(result,maxv(mid+1,j)-minv(i,mid));
+        return result;
+    }
     int maxProfit(vector<int>& prices) {
-        int currentmin = prices[0];
-        int maxPro = 0;
-        for(int i = 1;i<prices.size();i++){
-            currentmin = min(currentmin,prices[i]);
-            maxPro= max(maxPro,prices[i]-currentmin);
-        }
-        return maxPro;
-
+        p = prices;
+        return ans(0,p.size()-1);
     }
 };
