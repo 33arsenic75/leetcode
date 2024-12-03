f08eb60ec9e12afde31d5da4d14717328aaac3df
Abhinav Rajesh Shripad <122697564+33arsenic75@users.noreply.github.com>
2024-12-03 13:17:49 +0530

    Time: 3 ms (52.2%), Space: 60.9 MB (10.72%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -1,34 +1,63 @@
+// class Solution {
+// public:
+//     int longestNiceSubarray(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>>bits(n+1,vector<int>(32,0));
+//         for(int i = 0 ; i < n ; i++){
+//             for(int j = 0 ; j < 32 ; j++){
+//                 bits[i+1][j] = bits[i][j]+((nums[i]>>j)&1);
+//             }
+//         }
+//         function<bool(int,int)>check = [&](int start, int end){
+//             for(int j = 0 ; j < 32 ; j++){
+//                 if(bits[end][j]-bits[start-1][j]>1)return false;
+//             }
+//             return true;
+//         };
+//         int result = 1;
+//         for(int i = 1 ; i <= n ; i++){
+//             int high = n, low = i+1, mid = 0, ans = 0;
+//             while(low<=high){
+//                 mid = low + (high-low)/2;
+//                 if(check(i,mid)){
+//                     ans = mid;
+//                     low = mid + 1;
+//                 }
+//                 else{
+//                     high = mid - 1;
+//                 }
+//             }
+//             result = max(result,ans-i+1);
+//         }
+//         return result;
+//     }
+// };
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>bits(n+1,vector<int>(32,0));
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < 32 ; j++){
-                bits[i+1][j] = bits[i][j]+((nums[i]>>j)&1);
-            }
-        }
-        function<bool(int,int)>check = [&](int start, int end){
-            for(int j = 0 ; j < 32 ; j++){
-                if(bits[end][j]-bits[start-1][j]>1)return false;
-            }
-            return true;
-        };
-        int result = 1;
-        for(int i = 1 ; i <= n ; i++){
-            int high = n, low = i+1, mid = 0, ans = 0;
-            while(low<=high){
-                mid = low + (high-low)/2;
-                if(check(i,mid)){
-                    ans = mid;
-                    low = mid + 1;
-                }
-                else{
-                    high = mid - 1;
+            
+            int s=0,e=0;
+            int ans=0;
+            int n=nums.size();
+            int prev=0;
+            
+            while(e<n){
+                if((nums[e] & prev) == 0){
+                    prev|=nums[e];
+                    ans=max(ans,e-s+1);
+                    e++;
+                    
+                }else{
+                    while(s<e && (nums[e]&prev)!=0){
+                        prev&=(~nums[s]);
+                        s++;
+                    }
+
+                    prev|=nums[e];
+                    e++;
                 }
             }
-            result = max(result,ans-i+1);
-        }
-        return result;
+            return ans;
+
     }
 };
