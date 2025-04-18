e28ab64458ebcfbc377d2006e223dc1c8a7b87eb
Abhinav Rajesh Shripad <122697564+33arsenic75@users.noreply.github.com>
2025-04-18 09:18:45 +0530

    Time: 4 ms (56.36%), Space: 11.1 MB (23.5%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,50 +1,33 @@
-// class Solution {
-// public:
-//     string countAndSay(int n) {
-//         if(n==1){
-//             return "1";
-//         }
-//         string ans;
-//         string s = countAndSay(n-1) + " ";
-//         int cnt = 1;
-//         char c = s[0];
-//         for(int i = 1 ; i < s.size() ; i++){
-//             if(s[i]==c){
-//                 cnt++;
-//             }
-//             else{
-//                 ans += to_string(cnt) + c;
-//                 c = s[i];
-//                 cnt = 1;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
-    string countAndSay(int n) {
-        vector<string>ans(n+1);
-        ans[1]="1";
-        for(int i = 2; i <= n ; i++){
-            string result;
-            string s = ans[i-1] + " ";
-            int cnt = 1;
-            char c = s[0];
-            for(int i = 1 ; i < s.size() ; i++){
-                if(s[i]==c){
-                    cnt++;
-                }
-                else{
-                    result += to_string(cnt) + c;
-                    c = s[i];
-                    cnt = 1;
+    using info = pair<int, char>; // (frequency, digit)
+    static string str(const vector<info>& fc) {
+        string ans;
+        ans.reserve(fc.size()<<1);
+        for (auto& [f, c] : fc) {
+            ans+= to_string(f);
+            ans.push_back(c);
+        }
+        return ans;
+    }
+    static string countAndSay(int n) {
+        if (n==1) return "1";
+        vector<info> fc={{1, '1'}};
+        string s1=str(fc);
+        for (int i=3; i<=n; i++) {
+            const int sz=s1.size();
+            char c=s1[0];
+            fc={{1, c}};
+            for (int i=1; i<sz; i++) {
+                if (s1[i]==c)
+                    fc.back().first++;
+                else {
+                    fc.emplace_back(1, s1[i]);
+                    c=s1[i];
                 }
             }
-            ans[i] = result;
+            s1=str(fc);
         }
-        return ans[n];
+        return s1;
     }
 };
-
