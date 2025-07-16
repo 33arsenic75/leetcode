5ac4856d8a4bfc85de09352c195059a6b1efe94e
Abhinav Rajesh Shripad <122697564+33arsenic75@users.noreply.github.com>
2025-07-16 19:10:56 +0530

    Time: 7 ms (50%), Space: 96.1 MB (82.65%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -3,30 +3,16 @@ public:
     int maximumLength(vector<int>& nums) {
         int n = nums.size();
         for(int i = 0 ; i < n ; i++)nums[i] %= 2;
-        int lo = -1;
-        int lz = -1;
-        int z = 0, o = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] == 0){
-                lz = i;
-                z++;
-            }
-            else{
-                lo = i;
-                o++;
+        int o = count(nums.begin(), nums.end(), 1);
+        int z = count(nums.begin(), nums.end(), 0);
+        int last = 1 - nums[0];
+        int len = 1;
+        for(int i = 1 ; i < n ; i++){
+            if(nums[i] == last){
+                len++;
+                last = 1 - last;
             }
         }
-        if(lo == -1 || lz == -1)return max(z,o);
-        int llo = 0, llz = 0;
-        for(int i = max(lo,lz); i >= 0 ; i--){
-            if(nums[i] == 0){
-                llz = llo + 1;
-            }
-            else{
-                llo = llz + 1;
-            }
-        }
-
-        return max(max(llo,llz),max(z,o));
+        return max(len, max(z, o));
     }
 };
