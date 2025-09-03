f97af378a60fbd8d28da83ff01b84d7c6ca8896a
Abhinav Rajesh Shripad <122697564+33arsenic75@users.noreply.github.com>
2025-09-03 22:43:57 +0530

    Time: 41 ms (79.8%), Space: 21.1 MB (14.58%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid.size(),m = grid[0].size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        int dx[3] = {-1,0,1};
-        function<int(int,int,int)>f = [&](int d,int i,int j){
-            if(i<0 || j<0 || i>= m || j>= m)return 0;
-            else if(d<0 || d>= n)return 0;
-            else if(dp[d][i][j]!=-1)return dp[d][i][j];
-            int best = 0;
-            for(int x1: dx){
-                for(int x2: dx){
-                    best = max(best,f(d+1,i+x1,j+x2));
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(m, vector<int>(m,INT_MIN)));
+        dp[0][0][m-1] = grid[0][0] + grid[0][m-1];
+        for(int i = 1 ; i < n ; i++){
+            for(int p1 = 0 ; p1 < m ; p1++){
+                for(int p2 = 0 ; p2 < m ; p2++){
+                    int best = INT_MIN;
+                    for(int p1old = max(0,p1-1) ; p1old < min(m, p1+2) ; p1old++){
+                        for(int p2old = max(0,p2-1) ; p2old < min(m, p2+2) ; p2old++){
+                            best = max(best, dp[i-1][p1old][p2old]);
+                        }
+                    }
+                    if(best!=INT_MIN && p1!=p2)dp[i][p1][p2]=best+grid[i][p1]+grid[i][p2];
+                    else if(best!=INT_MIN)dp[i][p1][p2]=best+grid[i][p1];
                 }
             }
-            if(i!=j)dp[d][i][j] = grid[d][i] + grid[d][j] + best;
-            else dp[d][i][j] = grid[d][i] + best;
-            return dp[d][i][j];
-        };
-        int ans = f(0,0,m-1);
+        }
+        int ans = 0;
+        for(int p1 = 0 ; p1 < m ; p1++){
+            for(int p2 = 0 ; p2 < m ; p2++)ans=max(ans, dp[n-1][p1][p2]);
+        }
         return ans;
     }
 };
