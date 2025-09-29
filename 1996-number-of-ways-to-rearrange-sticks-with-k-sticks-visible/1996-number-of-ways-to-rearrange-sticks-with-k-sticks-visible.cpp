89749ccd140ccda5e5d157cc1d19355fabab35db
Abhinav Rajesh Shripad <122697564+33arsenic75@users.noreply.github.com>
2025-09-29 21:10:23 +0530

    Time: 89 ms (45.34%), Space: 122.4 MB (14.41%) - LeetHub

---
+++ 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
@@ -0,0 +1,16 @@
+using ll = long long;
+class Solution {
+public:
+    const ll mod = 1e9 + 7;
+    int rearrangeSticks(int n, int k) {
+        vector<vector<ll>> dp(n+1, vector<ll>(k+1, 0));
+        dp[0][0] = 1; // base case
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                dp[i][j] = (dp[i-1][j-1] + (ll)(i-1) * dp[i-1][j]) % mod;
+            }
+        }
+        return dp[n][k];
+    }
+};
